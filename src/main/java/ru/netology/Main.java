1860197a6c5565015d99d8aac18d109e01287225
KristinaKac <kachalkovakristina@mailru>
2022-07-10 21:29:02 +0300

    first commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,94 @@
+package ru.netology;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        // Работа с CSV файлом
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json);
+        // Работа с XML файлом
+        String fileName2 = "data.xml";
+        List<Employee> list2 = parseXML(fileName2);
+    }
+    private static List<Employee> parseXML(String fileName2) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(fileName2);
+
+        Node root = document.getDocumentElement();
+        System.out.println("Корневой элемент: " + root.getNodeName());
+
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node_.getNodeType()) {
+                System.out.println("Текущий узел: " + node_.getNodeName());
+                Element element = (Element) node_;
+                NamedNodeMap map = element.getAttributes();
+                for (int a = 0; a < map.getLength(); a++) {
+                    String attrName = map.item(a).getNodeName();
+                    String attrValue = map.item(a).getNodeValue();
+                    System.out.println("Атрибут: " + attrName + "; значение: " + attrValue);
+                }
+            }
+        }
+      return list;
+    }
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            List<Employee> list = csv.parse();
+            list.forEach(System.out::println);
+            return list;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private static String listToJson(List<Employee> list){
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        String json = gson.toJson(list, listType);
+        System.out.println(json);
+        return json;
+    }
+    private static void writeString(String json){
+        try (FileWriter writer = new FileWriter("data.json")){
+            writer.write(json);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
